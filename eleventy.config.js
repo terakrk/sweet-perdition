2bb5fc164896dc78daee8a9386470c9cbec8ad13
Tera Kirk <terakrk@gmail.com>
2024-05-05 21:21:47 -0500

    central image directory again

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,15 @@
 
 // importing from config
 const {getBlog} = require('./config/collections/index.js');
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
+// import filters
+const markdownFilter = require('./config/filters/markdown-filter.js');
 
 module.exports = eleventyConfig => {
+
+  // Filters
+  eleventyConfig.addFilter("markdownFilter", markdownFilter);
 
   // Collections
   eleventyConfig.addCollection('blog', getBlog);
@@ -12,7 +19,9 @@ module.exports = eleventyConfig => {
       );
       
     // Images
-  eleventyConfig.addPassthroughCopy('**/*.jpg');
+    ['src/assets/images/'].forEach(path =>
+        eleventyConfig.addPassthroughCopy(path)
+  );
 
     return {
         dir: {
